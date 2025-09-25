079c5fe9ca34f67ded42f36a640d80480d3bb0b3
Johan192004 <johanruma@gmail.com>
2025-09-24 23:29:08 -0500

    feat: completly implements the activity 7 from day 2

--- src/dia2/activity7/Main.java
+++ src/dia2/activity7/Main.java
@@ -3,12 +3,55 @@ package dia2.activity7;
 import java.util.HashMap;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        HashMap<String,User> users = new HashMap<>();
 
-        
+    public static void main(String[] args) {
+
+        HashMap<String, User> users = new HashMap<>();
+
+        User user1 = new User("1", "Johan");
+        User user2 = new User("2", "Maria");
+        User user3 = new User("3", "Carlos");
+
+        // storing users
+        users.put(user1.key(), user1);
+        users.put(user2.key(), user2);
+        users.put(user3.key(), user3);
+
+        System.out.println("Usuarios en el sistema:");
+        for (User user : users.values()) {
+            System.out.println(user);
+        }
+
+        // seaching for a user
+        String searchId = "2";
+        User foundUser = users.get(searchId);
+        if (foundUser != null) {
+            System.out.println("\nUsuario encontrado: " + foundUser);
+        } else {
+            System.out.println("\nUsuario no encontrado.");
+        }
+
+        // updating a user
+        User userToUpdate = users.get("3");
+        if (userToUpdate != null) {
+            userToUpdate.setName("Pedro");
+            System.out.println("\nUsuario actualizado: " + userToUpdate);
+        } else {
+            System.out.println("\nUsuario no encontrado para actualizar.");
+        }
+
+        // removing a user
+        User removedUser = users.remove("1");
+        if (removedUser != null) {
+            System.out.println("\nUsuario eliminado: " + removedUser);
+        } else {
+            System.out.println("\nUsuario no encontrado para eliminar.");
+        }
+
+        System.out.println("\nUsuarios restantes en el sistema:");
+        for (User user : users.values()) {
+            System.out.println(user);
+        }
 
     }
 }
--- src/dia2/activity7/User.java
+++ src/dia2/activity7/User.java
@@ -15,7 +15,7 @@ public class User implements Storable{
 
     @Override
     public String key() {
-        
+        return id;
     }
 
 
@@ -40,6 +40,11 @@ public class User implements Storable{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", name=" + name;
     }
 
     
